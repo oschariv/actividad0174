9855525353b559599621e31cd7cfc693aa269971
Oscar Charro Rivera <oschariv@gmail.com>
2017-02-09 09:56:06 +0100

    creadas clases

---
+++ Persona.java
@@ -0,0 +1,31 @@
+
+/**
+ * Write a description of class Persona here.
+ * 
+ * @author oschariv(Oscar Charro Rivera) 
+ * @version 1.0(09-02-2017)
+ */
+public class Persona
+{
+    
+    /**
+     * Constructor for objects of class Persona
+     * Si el booleano hombre es igual true la persona es un hombre y si es false 
+     * la persona es una mujer
+     */
+    public Persona(String nombre, boolean hombre, int peso, int altura, int edad)
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void sampleMethod(int y)
+    {
+        
+    }
+}
